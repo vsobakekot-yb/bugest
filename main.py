fc57f2c865bdc418e2b2a9ef7b55854d9b73c693
vsobakekot-yb <79642903+vsobakekot-yb@users.noreply.github.com>
2021-02-25 15:32:57 +0300

    upload
    bla bla...

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    """
+    The function that runs at the module execute moment.
+
+    Keyword arguments:
+    --parameters from 'Settings and Results' or *args, **kwargs
+
+    Return:
+
+    dict where
+
+    keys -- ports
+    values -- data in ports
+    """
+
+    return {}
